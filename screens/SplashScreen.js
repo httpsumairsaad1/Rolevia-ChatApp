5444ed52e12f8efa07604a15ba9ebfc4a6680d68
Umair Saad <121795621+httpsumairsaad1@users.noreply.github.com>
2024-12-13 23:27:29 +0500

    Update SplashScreen.js

--- screens/SplashScreen.js
+++ screens/SplashScreen.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import { useNavigation } from '@react-navigation/native';
+import React, { useEffect } from 'react';
 import { View, Text, Image, StyleSheet } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient'; // Ensure you have react-native-linear-gradient installed
 
 const SplashScreen = () => {
+
+const navigation = useNavigation()
+  useEffect(() => {
+      setTimeout(() => {
+          navigation.navigate('Slider')
+      }, 2500);
+  }, [])
+  
+
   return (
     <LinearGradient 
       colors={['#3C4D57', '#82A7BD']} 
       style={styles.container}
     >
       <Image 
-        source={require('./assets/logo-rolevia.png')} // Replace with your logo path
+        source={require('../assets/logo-rolevia.png')} // Replace with your logo path
         style={styles.logo}
         resizeMode="contain"
       /> 
